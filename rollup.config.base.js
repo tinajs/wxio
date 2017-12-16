81add28996678e6406e995d20502d3ee043bf0a0
yelo <zhihuzeye@gmail.com>
2017-12-17 04:19:07 +0800

    build: use babel runtime by default

--- rollup.config.base.js
+++ rollup.config.base.js
@@ -1,25 +1,33 @@
 import commonjs from 'rollup-plugin-commonjs'
 import nodejs from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
 
-const cssExportMap = {}
+export const babelrc = {
+  presets: [
+    [
+      'env',
+      {
+        targets: {
+          browsers: ['last 2 versions'],
+        },
+        modules: false,
+      },
+    ],
+    'stage-0',
+  ],
+  plugins: [
+    'external-helpers',
+  ],
+}
 
-// compile some libraries of sindresorhus with babel, because these are using es2015+ syntax but with commonjs.
 export default {
   input: 'src/index.js',
+  output: {
+    name: 'wxio',
+  },
   plugins: [
     nodejs(),
     commonjs({
       exclude: 'src/**',
-    }),
-    babel({
-      include: [
-        'src/**',
-        'node_modules/map-obj/**',
-        'node_modules/p-cancelable/**',
-        'node_modules/p-queue/**',
-        'node_modules/p-timeout/**',
-      ],
     }),
   ],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,25 @@
+import babel from 'rollup-plugin-babel'
 import merge from 'deepmerge'
-import base from './rollup.config.base.js'
+import base, { babelrc } from './rollup.config.base'
 
+// compile some libraries of sindresorhus with babel, because these are using es2015+ syntax but with commonjs.
 export default merge(base, {
   output: {
     file: 'dist/wxio.js',
     format: 'umd',
-    name: 'wxio',
+  },
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: babelrc.presets,
+      plugins: merge(babelrc.plugins, [
+        'transform-runtime',
+      ]),
+      include: '**',
+      runtimeHelpers: true,
+    }),
+  ],
+  external: (id) => {
+    return /^babel-runtime(\/.*)?$/.test(id)
   },
 })
