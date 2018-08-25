e4d3e73ebc3276bb3ae81969ad22d2eba3a4fccb
ambar <ambar.lee@gmail.com>
2018-08-25 12:37:55 +0800

    build: compile all files with `babel.config.js`

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,6 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
-import fs from 'fs'
-
-const babelrc = JSON.parse(fs.readFileSync('./.babelrc', 'utf8'))
 
 export default [
   {
@@ -17,13 +14,8 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      babel({
-        // compile some libraries of sindresorhus with babel, because these are using es2015+ syntax but with commonjs.
-        // see: https://github.com/rollup/rollup-plugin-babel#external-dependencies
-        ...babelrc,
-        babelrc: false,
-        include: '**',
-      }),
+      // compile all files (including dependencies) with `babel.config.js`
+      babel(),
     ],
   },
 ]
