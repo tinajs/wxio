fc8023895e0f3e2840eb7a2a1c785402745254e5
ambar <ambar.lee@gmail.com>
2018-08-27 14:46:02 +0800

    build: add cjs/esm build

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,44 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import mergeWith from 'lodash/mergeWith'
+import partialRight from 'lodash/partialRight'
 import pkg from './package.json'
+
+const deepMerge = partialRight(mergeWith, (dest, src) => {
+  if (Array.isArray(dest)) {
+    return dest.concat(src)
+  }
+})
+
+const baseConfig = {
+  input: 'src/index.js',
+  plugins: [
+    resolve(),
+    commonjs(),
+    // compile all files (including dependencies) with `babel.config.js`
+    babel()
+  ]
+}
 
 export default [
   {
-    input: 'src/index.js',
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+    },
+  },
+  {
+    output: {
+      file: pkg.module,
+      format: 'esm',
+    },
+  },
+  {
     output: {
       name: pkg.name,
-      file: pkg.main,
+      file: 'dist/wxio.js',
       format: 'umd',
     },
-    plugins: [
-      resolve(),
-      commonjs(),
-      // compile all files (including dependencies) with `babel.config.js`
-      babel(),
-    ],
   },
-]
+].map((config) => deepMerge({}, baseConfig, config))
